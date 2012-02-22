df66234fe3c3e46ff26b2d98a87d971cc26923e8
Hoang Xuan Phu <phunehehe@gmail.com>
2012-02-22 11:29:02 +0700

    fix some compatibility issues

--- rcdiff.sh
+++ rcdiff.sh
@@ -1,2 +1,8 @@
-#!/bin/sh
-for i in _*; do diff $i ~/${i/_/.}; done
+#!/bin/bash
+for config_file in _*
+do
+    home_file=$HOME/${config_file/_/.}
+    diff -q $config_file $home_file
+    different=$?
+    [[ $different == 1 ]] && cp $home_file $config_file
+done
