4ca9550958f36d0efaa3cd863470e6cbe1581e74
Hoang Xuan Phu <phunehehe@gmail.com>
2012-08-25 19:29:52 +0700

    vim to use symlink

--- vim/install.sh
+++ vim/install.sh
@@ -1,76 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 
 
-# Make backup(s) if nessesary then copy new config to $HOME
-function copy_file {
-
-    source_file="$1"
-    name="$(basename $source_file)"
-    target="$HOME/${name/_/.}"
-
-    if [ -e "$target" ]
-    then
-        backup="$target.bak"
-        if [ -e "$backup" ]
-        then
-            count=0
-            while [ -e "$backup.$count" ]; do let "count += 1"; done
-            backup="$backup.$count"
-        fi
-        mv "$target" "$backup"
-        echo "Backup file $backup created."
-    fi
-
-    echo "Creating config file $target."
-    mv "$source_file" "$target"
-}
+stamp=terminal-dotfiles
+now=$(date +'%Y-%m-%d_%H-%M-%S')
+bin_dir="$(cd "$(dirname "$0")" && pwd)"
 
 
-# Prepare the directories
+# Setup indie config files
 
-cwd="$(dirname $0)"
-
-vim_dir="$cwd/_vim"
-colors_dir="$vim_dir/colors"
-autoload_dir="$vim_dir/autoload"
-bundle_dir="$vim_dir/bundle"
-
-mkdir -p "$vim_dir" "$colors_dir" "$bundle_dir" "$autoload_dir"
-
-
-# Download and install plugins
-
-$curl > "$autoload_dir"/pathogen.vim \
-     https://github.com/tpope/vim-pathogen/raw/master/autoload/pathogen.vim
-$curl > "$colors_dir"/xoria256.vim \
-     https://github.com/vim-scripts/xoria256.vim/raw/master/colors/xoria256.vim
-
-plugins=(
-https://github.com/scrooloose/nerdtree/tarball/master
-https://github.com/kien/ctrlp.vim/tarball/master
-https://github.com/Lokaltog/vim-easymotion/tarball/master
-https://github.com/pangloss/vim-javascript/tarball/master
-https://github.com/kchmck/vim-coffee-script/tarball/master
-https://github.com/tpope/vim-fugitive/tarball/master
-https://github.com/tpope/vim-eunuch/tarball/master
-https://github.com/tpope/vim-surround/tarball/master
+dotfiles=(
+_vim
+_vimrc
 )
-for url in "${plugins[@]}"
+
+for dotfile in "${dotfiles[@]}"
 do
-    archive="$cwd/master.tar.gz"
-    $curl "$url" | tar -xz -C "$bundle_dir"
-done
-
-
-# And here comes the vimrc
-$curl > "$cwd"/_vimrc \
-     https://github.com/phunehehe/terminal-dotfiles/raw/master/vim/_vimrc \
-
-
-# The Great Move
-for item in "$cwd"/_*
-do
-    copy_file $item
+    destination="$HOME/${dotfile/_/.}"
+    [[ -h "$destination" ]] && rm "$destination"
+    [[ -e "$destination" ]] && mv "$destination" "$destination.$stamp-$now"
+    ln -s "$bin_dir/$dotfile" "$destination"
 done
