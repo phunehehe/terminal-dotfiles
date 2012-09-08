2c1885dd8ec42d5613272f373878d07fb914c1cf
Hoang Xuan Phu <phunehehe@gmail.com>
2012-09-08 11:52:30 +0700

    need rcdiff no more

--- rcdiff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for config_file in _*
-do
-    home_file=$HOME/${config_file/_/.}
-    diff -q $config_file $home_file
-    different=$?
-    [[ $different == 1 ]] && cp $home_file $config_file
-done
