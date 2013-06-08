729145557f2cac013f4f787793efd2f1a83554cc
Hoang Xuan Phu <phunehehe@gmail.com>
2013-06-09 00:06:53 +0700

    job count for zsh

--- generate-powerline.py
+++ generate-powerline.py
@@ -6,9 +6,37 @@ import os
 
 my_config = {
     'common': {
-        'term_truecolor': True
+        #'term_truecolor': True,
+        #'additional_escapes': 'tmux',
     }
 }
+
+my_theme_shell_default = {
+    'segments': {
+        'left': [
+            { 'name': 'hostname' },
+            { 'name': 'user' },
+            { 'name': 'virtualenv' },
+            {
+                'name': 'cwd',
+                'args': { 'dir_limit_depth': 3 },
+            },
+        ],
+        'right': [
+            {
+                'module': 'powerline.segments.shell',
+                'name': 'last_pipe_status',
+            },
+            {
+                'module': 'powerline.segments.shell',
+                'name': 'jobs',
+            },
+            { 'name': 'branch' },
+        ]
+    }
+}
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def merge(base_dict, extra_dict):
@@ -19,12 +47,17 @@ def merge(base_dict, extra_dict):
             base_dict[key] = extra_value
 
 
-this_dir = os.path.dirname(os.path.abspath(__file__))
 with open('%s/powerline/powerline/config_files/config.json' % this_dir) as default_file:
-    default_config = json.load(default_file)
+    config = json.load(default_file)
 
-
-config = default_config.copy()
 merge(config, my_config)
 with open('%s/_config/powerline/config.json' % this_dir, 'w') as config_file:
+    json.dump(config, config_file, indent=4, sort_keys=True)
+
+
+with open('%s/powerline/powerline/config_files/themes/shell/default.json' % this_dir) as default_file:
+    config = json.load(default_file)
+
+merge(config, my_theme_shell_default)
+with open('%s/_config/powerline/themes/shell/default.json' % this_dir, 'w') as config_file:
     json.dump(config, config_file, indent=4, sort_keys=True)
