76289ce4b021d3ec6652861fd599e4aa713886fc
Hoang Xuan Phu <phunehehe@gmail.com>
2013-06-10 23:06:22 +0700

    print current time

--- generate-powerline.py
+++ generate-powerline.py
@@ -16,9 +16,16 @@ my_config = {
     },
 }
 
-my_theme_shell_default = {
+my_shell_theme = {
     "segments": {
         "left": [
+            {
+                "name": "date",
+                "args": {
+                    "istime": True,
+                    "format": "%H:%M:%S",
+                },
+            },
             { "name": "hostname" },
             { "name": "user" },
             { "name": "virtualenv" },
@@ -39,12 +46,26 @@ my_theme_shell_default = {
     }
 }
 
+my_shell_colorscheme = {
+    "groups": {
+        "time": {
+            "fg": "gray10",
+            "bg": "gray2",
+            "attr": ["bold"],
+        },
+        "time:divider": {
+            "fg": "gray5",
+            "bg": "gray2",
+        },
+    }
+}
+
 this_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def merge(base_dict, extra_dict):
     for key, extra_value in extra_dict.iteritems():
-        if isinstance(extra_value, dict):
+        if key in base_dict and isinstance(extra_value, dict):
             merge(base_dict[key], extra_value)
         else:
             base_dict[key] = extra_value
@@ -52,7 +73,6 @@ def merge(base_dict, extra_dict):
 
 with open('%s/powerline/powerline/config_files/config.json' % this_dir) as default_file:
     config = json.load(default_file)
-
 merge(config, my_config)
 with open('%s/_config/powerline/config.json' % this_dir, 'w') as config_file:
     json.dump(config, config_file, indent=4, sort_keys=True)
@@ -60,7 +80,12 @@ with open('%s/_config/powerline/config.json' % this_dir, 'w') as config_file:
 
 with open('%s/powerline/powerline/config_files/themes/shell/default_leftonly.json' % this_dir) as default_file:
     config = json.load(default_file)
-
-merge(config, my_theme_shell_default)
+merge(config, my_shell_theme)
 with open('%s/_config/powerline/themes/shell/default_leftonly.json' % this_dir, 'w') as config_file:
+    json.dump(config, config_file, indent=4, sort_keys=True)
+
+with open('%s/powerline/powerline/config_files/colorschemes/shell/default.json' % this_dir) as default_file:
+    config = json.load(default_file)
+merge(config, my_shell_colorscheme)
+with open('%s/_config/powerline/colorschemes/shell/default.json' % this_dir, 'w') as config_file:
     json.dump(config, config_file, indent=4, sort_keys=True)
